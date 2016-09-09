e116fd79a1f072bfe63117c9de0a9f641bec0847
NarayananRG <narayananeeeb@gmail.com>
2016-09-09 06:56:49 +0530

    Update helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    char n;    
+    scanf("%c",&n);
+    if(n=='a'||n=='e'||n=='o'||n=='i'||n=='u') 
+    printf("vowel");
+    else
+    printf("consonant");
+    return 0;
+}
