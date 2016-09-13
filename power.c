dfae6fbabff72a05ed99f2396a30404b1cffa95b
NarayananRG <narayananeeeb@gmail.com>
2016-09-13 22:38:20 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int base,power;
+   scanf("%d%d",&base,&power);
+   int result=pow(base,power);
+   printf("%d",result);
+    return 0;
+}
