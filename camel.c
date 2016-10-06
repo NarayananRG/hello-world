b402f29210e442fffb7a67218e56f84b3afbc342
NarayananRG <narayananeeeb@gmail.com>
2016-10-06 19:57:12 +0530

    Update camel.c

--- camel.c
+++ camel.c
@@ -2,11 +2,11 @@
 int main()
 {
     char name[100];
-    int f, i, l;
+    int i,j,l;
     gets(name);
     if(name[0]>='a'&&name[0]<='z')
     {
-        name[0] = name[0]  - 32;
+        name[0] =name[0]-32;
     }
     for(i=1;i<l;i++)
      {
@@ -14,7 +14,7 @@ int main()
         {
             if(name[i+1]>='a'&& name[i+1]<='z')
             {
-              int j = i + 1;
+               j=i+1;
               name[j]= name[j]-32;
             }
         }
