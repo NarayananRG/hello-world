8c39f11da427b4cd9d426717631cfd2946ccb441
NarayananRG <narayananeeeb@gmail.com>
2018-05-31 21:56:37 -0700

    Add files via upload

---
+++ lucky number.c
@@ -0,0 +1,44 @@
+
+
+#include <stdio.h>
+
+
+int main()
+
+{
+    
+int num[100000];
+    
+int i,n;
+    
+printf("Enter the value of n: ");
+    
+scanf("%d",&n);
+    
+printf("\nEnter the values of num: ");
+    
+for(i=0;i<n;i++)
+    
+{
+        
+scanf("%d",&num[i]);
+            
+    
+}
+    
+printf("\nThe entered values are:");
+    
+for(i=0;i<n;i++)
+    
+{
+        
+if((i*num[i])==num[i])
+        
+printf("Lucky number: %d ",num[i]);
+            
+    
+}
+    
+return 0;
+
+}
